411062fea22af97138384885a725d342346e2862
Chris Catesby <chris.catesby@connect.auto>
2021-02-03 14:01:36 +0000

    Add ReactJS app. add vscode launch settings.

--- src/main/java/funds/spring/data/neo4j/SampleFundApplication.java
+++ src/main/java/funds/spring/data/neo4j/SampleFundApplication.java
@@ -1,6 +1,8 @@
 package funds.spring.data.neo4j;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -13,5 +15,14 @@ public class SampleFundApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SampleFundApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+            }
+        };
     }
 }
